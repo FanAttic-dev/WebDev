268532322654480a6f9d98f959a1300f626d3645
FanAttic <attic.zsiros@gmail.com>
2020-03-05 19:26:34 +0100

    logic done

--- 04JS/Color Game/script.js
+++ 04JS/Color Game/script.js
@@ -1,35 +1,74 @@
-var colors = generateColors(6);
-
-var pickedColor = pickColor();
-displayPickedColor();
-
 var messageDisplay = document.getElementById("message");
 var squares = document.getElementsByClassName("square");
+var pickedDisplay = document.getElementById("pickedColor");
+var resetButton = document.querySelector("#reset");
+var easyButton = document.querySelector("#easyBtn");
+var hardButton = document.querySelector("#hardBtn");
+var h1 = document.querySelector("h1");
 
-for (var i = 0; i < squares.length; i++) {
-	// add initial colors to squares
-	squares[i].style.backgroundColor = colors[i];
+var backgroundColor = "#232323";
+var colors = [];
+var pickedColor;
+var colorsCount = 6;
 
-	// add click listeners
-	squares[i].addEventListener("click", function() {
-		var clickedColor = this.style.backgroundColor;
+initCallbacks();
+resetGrid();
 
-		if (clickedColor === pickedColor) {
-			messageDisplay.textContent = "Correct!";
-			setAllColors(clickedColor)
-		} else {
-			this.style.visibility = "hidden";
-			messageDisplay.textContent = "TryAgain";
-		}
+function initCallbacks() {
+	resetButton.addEventListener("click", function() { resetGrid(); });
+	easyButton.addEventListener("click", function() {
+		easyButton.classList.add("selected");
+		hardButton.classList.remove("selected");
+
+		colorsCount = 3;
+		resetGrid();
 	});
-};
 
-function displayPickedColor() {
-	var pickedDisplay = document.getElementById("pickedColor");
+	hardButton.addEventListener("click", function() {
+		hardButton.classList.add("selected");
+		easyButton.classList.remove("selected");
+
+		colorsCount = 6;
+		resetGrid();
+	});
+
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].addEventListener("click", function() {
+			var clickedColor = this.style.backgroundColor;
+
+			if (clickedColor === pickedColor) {
+				messageDisplay.textContent = "Correct!";
+				resetButton.textContent = "Play Again?";
+				setAllColors(clickedColor)
+			} else {
+				this.style.backgroundColor = backgroundColor;
+				messageDisplay.textContent = "TryAgain";
+			}
+		});
+	};
+}
+
+function resetGrid() {
+	colors = generateColors(colorsCount);
+	pickedColor = pickColor();
+	
+	h1.style.backgroundColor = backgroundColor;
+	messageDisplay.textContent = "";
+	resetButton.textContent = "New Colors";
 	pickedDisplay.textContent = pickedColor;
+
+	for (var i = 0; i < squares.length; i++) {
+		if (i < colorsCount) {
+			squares[i].style.display = "initial";
+			squares[i].style.backgroundColor = colors[i];
+		} else {
+			squares[i].style.display = "none";
+		}
+	}
 }
 
 function setAllColors(color) {
+	h1.style.backgroundColor = color;
 	for (var i = 0; i < squares.length; i++) {
 		squares[i].style.backgroundColor = color;
 	}
