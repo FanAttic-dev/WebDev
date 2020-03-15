e62cd47cd205dc59028a239b133ce2e8222c1174
FanAttic <attic.zsiros@gmail.com>
2020-03-15 18:00:06 +0100

    auth finished

--- 05Backend/YelpCamp/app.js
+++ 05Backend/YelpCamp/app.js
@@ -33,6 +33,11 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+});
+
 app.get("/", function(req, res) {
     res.render("landing");
 });
@@ -88,7 +93,7 @@ app.get("/campgrounds/:id", function(req, res) {
 // COMMENTS ROUTES
 // ===============================================
 
-app.get("/campgrounds/:id/comments/new", function(req, res) {
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res) {
     Campground.findById(req.params.id, function(err, campground) {
         if (err) {
             console.log(err);
@@ -98,7 +103,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res) {
     });
 });
 
-app.post("/campgrounds/:id/comments", function(req, res) {
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res) {
     Campground.findById(req.params.id, function(err, campground) {
         if (err) {
             console.log(err);
@@ -152,7 +157,17 @@ app.post(
     })
 );
 
-//function isLoggedIn()
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/campgrounds");
+});
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
