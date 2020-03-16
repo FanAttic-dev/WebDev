03741030249a1dada13ecd9a202f5f7ef67543f4
FanAttic <attic.zsiros@gmail.com>
2020-03-16 15:00:12 +0100

    comment delete

--- 05Backend/YelpCamp/routes/comments.js
+++ 05Backend/YelpCamp/routes/comments.js
@@ -68,6 +68,20 @@ router.put("/:comment_id", function(req, res) {
     });
 });
 
+// DESTROY
+router.delete("/:comment_id", function(req, res) {
+    Comment.findByIdAndRemove(req.params.comment_id, function(
+        err,
+        deletedComment
+    ) {
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
 // MIDDLEWARE
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
